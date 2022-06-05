e908c06f6655770f7536fa3f86d6ac2812253e26
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2022-06-06 01:28:04 +0200

    Add some exit and init behavior to wezterm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,17 +1,24 @@
+-- Set nice hotkeys up with this! https://wezfurlong.org/wezterm/config/keys.html#leader-key
+
 local wezterm = require 'wezterm';
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  default_prog = {"wsl.exe", "--exec", "/usr/bin/fish"}
+  default_prog = {"wsl.exe", "--exec", "fish"}
 else
-  default_prog = {"/usr/local/bin/fish"}
+  default_prog = {"fish"}
 end
 
 return {
   color_scheme = "Solarized Darcula",
-  exit_behavior = "Close", -- remove after new release, "CloseOnCleanExit" is the default!
   default_prog = default_prog,
+  exit_behavior = "Close", -- remove after new release, "CloseOnCleanExit" is the default!
+  initial_cols = 120,
+  initial_rows = 36,
+  window_close_confirmation = "NeverPrompt",
+  keys = {
+    {key="w", mods="CTRL|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=false}}}, -- don't confirm closing tabs
+  },
 }
 
 -- Add this option when new release comes out!
---  clean_exit_codes = {130}, -- allows ^D after ^C in zsh without annoying message
-
+--  clean_exit_codes = {130}, -- allows ^D after ^C (exit code >0 from last command) in zsh without annoying message
