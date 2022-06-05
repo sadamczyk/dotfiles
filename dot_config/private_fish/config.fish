85e0541f1da812e307c099a125a67d317c58b661
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2022-06-06 01:13:21 +0200

    Add docker-compose build alias to fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -7,6 +7,7 @@ if status is-interactive
     abbr -a -U -- dc docker-compose
     abbr -a -U -- dcu 'docker-compose up -d'
     abbr -a -U -- dcub 'docker-compose up --build -d'
+    abbr -a -U -- dcb 'docker-compose build'
     abbr -a -U -- dcd 'docker-compose down'
     abbr -a -U -- dps 'docker ps'
 end
