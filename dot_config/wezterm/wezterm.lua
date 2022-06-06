7f6d733b6a32d1f532491f9243b7db81ccdaa6a7
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2022-06-06 15:33:43 +0200

    Automaticall switch wezterm color scheme along with OS

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -8,8 +8,27 @@ else
   default_prog = {"/usr/local/bin/fish"}
 end
 
+-- Automatically switch colorscheme based on OS appearance
+-- https://wezfurlong.org/wezterm/config/lua/window/get_appearance.html
+function scheme_for_appearance(appearance)
+  if appearance:find("Dark") then
+    return "Afterglow"
+  else
+    return "Alabaster"
+  end
+end
+
+wezterm.on("window-config-reloaded", function(window, pane)
+  local overrides = window:get_config_overrides() or {}
+  local appearance = window:get_appearance()
+  local scheme = scheme_for_appearance(appearance)
+  if overrides.color_scheme ~= scheme then
+    overrides.color_scheme = scheme
+    window:set_config_overrides(overrides)
+  end
+end)
+
 return {
-  color_scheme = "Solarized Darcula",
   default_prog = default_prog,
   exit_behavior = "Close", -- remove after new release, "CloseOnCleanExit" is the default!
   initial_cols = 500,
