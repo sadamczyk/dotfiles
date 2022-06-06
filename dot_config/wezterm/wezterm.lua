308336430e6c56f7626b72ee8f4dd38fe566affd
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2022-06-06 10:53:14 +0200

    Fix fish path for wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -3,17 +3,17 @@
 local wezterm = require 'wezterm';
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-  default_prog = {"wsl.exe", "--exec", "fish"}
+  default_prog = {"wsl.exe", "--exec", "/usr/bin/fish"}
 else
-  default_prog = {"fish"}
+  default_prog = {"/usr/local/bin/fish"}
 end
 
 return {
   color_scheme = "Solarized Darcula",
   default_prog = default_prog,
   exit_behavior = "Close", -- remove after new release, "CloseOnCleanExit" is the default!
-  initial_cols = 120,
-  initial_rows = 36,
+  initial_cols = 500,
+  initial_rows = 500,
   window_close_confirmation = "NeverPrompt",
   keys = {
     {key="w", mods="CTRL|SHIFT", action=wezterm.action{CloseCurrentTab={confirm=false}}}, -- don't confirm closing tabs
