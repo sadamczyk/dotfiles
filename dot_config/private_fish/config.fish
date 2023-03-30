4affd2f28c395b1ac227d7a9c6487d4799cdd0a3
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2023-03-30 22:56:23 +0200

    Update config.fish

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,18 +1,17 @@
 if status is-interactive
-    # General abbreviations
-    abbr -a -U -- l ll
-    abbr -a -U -- conf cd ~/.config/
-    abbr -a -U -- chm chezmoi
-    abbr -a -U -- g git
+    # General abbreviations 
+    abbr -a -- conf cd ~/.config/
+    abbr -a -- chm chezmoi
+    abbr -a -- g git
 
     ## Docker
-    abbr -a -U -- d docker
-    abbr -a -U -- dc docker compose
-    abbr -a -U -- dcu 'docker compose up -d'
-    abbr -a -U -- dcub 'docker compose up --build -d'
-    abbr -a -U -- dcb 'docker compose build'
-    abbr -a -U -- dcd 'docker compose down'
-    abbr -a -U -- dps 'docker ps'
+    abbr -a -- d docker
+    abbr -a -- dc docker compose
+    abbr -a -- dcu 'docker compose up -d'
+    abbr -a -- dcub 'docker compose up --build -d'
+    abbr -a -- dcb 'docker compose build'
+    abbr -a -- dcd 'docker compose down'
+    abbr -a -- dps 'docker ps'
 
     # Color theme
     fish_config theme choose Bay\ Cruise
@@ -21,8 +20,6 @@ if status is-interactive
     # -R for color, -x4 for tabs = 4 spaces
     set -gx LESS "-Rx4"
 end
-
-export PATH="/usr/local/bin:$PATH"
 
 # Load additional config not tracked by chezmoi
 source ~/.config/fish/local_config.fish
