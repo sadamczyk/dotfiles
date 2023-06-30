521e7cbe79c421a4b36f9cc357b0c237e13d6d63
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-06-30 15:57:54 +0200

    Remove unused git function. I prefer to use simple abbreviation

--- dot_config/private_fish/functions/git.fish
+++
@@ -1,8 +0,0 @@
-function g -d "Runs git status without args, or git when args are given."
-	# https://stackoverflow.com/a/29643375
-	if count $argv > /dev/null
-		git $argv
-	else
-		git status
-	end
-end
