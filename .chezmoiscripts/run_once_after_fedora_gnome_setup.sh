2004b41dd31b66deb8abab85fd586a98c92d520d
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-10-23 22:38:50 +0200

    Add fedora/GNOME specific setup script

---
+++ .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+test -f /usr/bin/open || sudo ln -s /usr/bin/xdg-open /usr/bin/open
+
+sudo dnf install -y \
+  geary \
+  google-chrome-stable
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak install -y io.dbeaver.DBeaverCommunity
