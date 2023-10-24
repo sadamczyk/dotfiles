f78567d446c50c4135c22450a595385401724d96
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-10-24 11:37:54 +0200

    DBeaver: Install manually via RPM package
    Minor cleanups

--- .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
+++ .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
@@ -2,13 +2,15 @@
 
 test -f /usr/bin/open || sudo ln -s /usr/bin/xdg-open /usr/bin/open
 
-sudo dnf copr enable wezfurlong/wezterm-nightly
-sudo dnf install -y wezterm
-
+sudo dnf copr enable -y wezfurlong/wezterm-nightly
 sudo dnf install -y \
   geary \
-  google-chrome-stable
+  google-chrome-stable \
+  wezterm
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak install -y io.dbeaver.DBeaverCommunity
 
+# Install DBeaver
+curl -LO https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm
+sudo dnf localinstall -y dbeaver-ce-latest-stable.x86_64.rpm
+rm dbeaver-ce-latest-stable.x86_64.rpm
