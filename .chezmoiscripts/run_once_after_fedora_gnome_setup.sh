2ca71f2a1ed601332da071cce72b0a8c438f5706
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-10-25 23:07:07 +0200

    I hate bash.

--- .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
+++ .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
@@ -19,14 +19,14 @@ rm dbeaver-ce-latest-stable.x86_64.rpm
 
 # Install Docker-CE
 # https://docs.docker.com/engine/install/fedora/
+# TODO Add $USER to docker group?
+# The sed command fixes extremely slow RPM package installation (yum etc.);
+# fixed in the docker images of newer RPM-based distros: https://github.com/rpm-software-management/rpm/commit/5e6f05cd8dad6c1ee6bd1e6e43f176976c9c3416
 sudo dnf -y install dnf-plugins-core \
   && sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo \
   && sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-  # TODO Add $USER to docker group?
   && sudo systemctl enable docker.service \
   && sudo systemctl enable containerd.service \
-  # Fix extremely slow RPM package installation (yum etc.)
-  # Fixed in the docker images of newer RPM-based distros: https://github.com/rpm-software-management/rpm/commit/5e6f05cd8dad6c1ee6bd1e6e43f176976c9c3416
   && sudo sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1024/' /usr/lib/systemd/system/docker.service
 
 # Install Google Cloud CLI
