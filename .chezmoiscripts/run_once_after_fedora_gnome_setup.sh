5113462e997a84d3ee1c295e64baa9b5f49d3a64
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-10-25 22:01:41 +0200

    Fedora: Install Google Cloud CLI

--- .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
+++ .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
@@ -27,3 +27,18 @@ sudo dnf -y install dnf-plugins-core \
   # Fixed in the docker images of newer RPM-based distros: https://github.com/rpm-software-management/rpm/commit/5e6f05cd8dad6c1ee6bd1e6e43f176976c9c3416
   && sudo sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1024/' /usr/lib/systemd/system/docker.service
 
+# Install Google Cloud CLI
+# https://cloud.google.com/sdk/docs/install-sdk?hl=de#rpm
+sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-cli]
+name=Google Cloud CLI
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=0
+gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM \
+  && sudo dnf install -y libxcrypt-compat.x86_64 \
+  && sudo dnf install -y google-cloud-cli google-cloud-cli-skaffold google-cloud-cli-gke-gcloud-auth-plugin \
+  && gcloud init
+
