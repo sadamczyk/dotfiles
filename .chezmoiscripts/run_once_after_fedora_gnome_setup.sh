767561eb9e0c1516fef3c362117cb1f40dc706cd
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-10-28 23:02:37 +0200

    Fedora: Add user to docker group

--- .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
+++ .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
@@ -27,12 +27,12 @@ rm dbeaver-ce-latest-stable.x86_64.rpm
 
 # Install Docker-CE
 # https://docs.docker.com/engine/install/fedora/
-# TODO Add $USER to docker group?
 # The sed command fixes extremely slow RPM package installation (yum etc.);
 # fixed in the docker images of newer RPM-based distros: https://github.com/rpm-software-management/rpm/commit/5e6f05cd8dad6c1ee6bd1e6e43f176976c9c3416
 dnf -y install dnf-plugins-core \
   && dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo \
   && dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+  && sudo usermod -aG docker $USER \
   && systemctl enable docker.service \
   && systemctl enable containerd.service \
   && sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1024/' /usr/lib/systemd/system/docker.service
