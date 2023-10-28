948f8d06cf5cc2173a507b198a8803ba49174afc
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-10-28 22:53:06 +0200

    Fedora: su to root, remove sudo everywhere

--- .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
+++ .chezmoiscripts/run_once_after_fedora_gnome_setup.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Ask for password once and then keep sudo alive
+# Switch to root
+sudo su
+
+# Ask for password once and then keep alive
 # https://serverfault.com/a/833888
-trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
+# trap "exit" INT TERM; trap "kill 0" EXIT; -v || exit $?; sleep 1; while true; do sleep 60; -nv; done 2>/dev/null &
 
-sudo dnf upgrade --refresh -y
+dnf upgrade --refresh -y
 
-test -f /usr/bin/open || sudo ln -s /usr/bin/xdg-open /usr/bin/open
+test -f /usr/bin/open || ln -s /usr/bin/xdg-open /usr/bin/open
 
-sudo dnf copr enable -y wezfurlong/wezterm-nightly
-sudo dnf config-manager --set-enabled google-chrome
-sudo dnf install -y \
+dnf copr enable -y wezfurlong/wezterm-nightly
+dnf config-manager --set-enabled google-chrome
+dnf install -y \
   geary \
   google-chrome-stable \
   wezterm
 
-sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 # Install DBeaver
 curl -LO https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm
-sudo dnf localinstall -y dbeaver-ce-latest-stable.x86_64.rpm
+dnf localinstall -y dbeaver-ce-latest-stable.x86_64.rpm
 rm dbeaver-ce-latest-stable.x86_64.rpm
 
 # Install Docker-CE
@@ -27,16 +30,16 @@ rm dbeaver-ce-latest-stable.x86_64.rpm
 # TODO Add $USER to docker group?
 # The sed command fixes extremely slow RPM package installation (yum etc.);
 # fixed in the docker images of newer RPM-based distros: https://github.com/rpm-software-management/rpm/commit/5e6f05cd8dad6c1ee6bd1e6e43f176976c9c3416
-sudo dnf -y install dnf-plugins-core \
-  && sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo \
-  && sudo dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
-  && sudo systemctl enable docker.service \
-  && sudo systemctl enable containerd.service \
-  && sudo sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1024/' /usr/lib/systemd/system/docker.service
+dnf -y install dnf-plugins-core \
+  && dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo \
+  && dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin \
+  && systemctl enable docker.service \
+  && systemctl enable containerd.service \
+  && sed -i 's/LimitNOFILE=infinity/LimitNOFILE=1024/' /usr/lib/systemd/system/docker.service
 
 # Install Google Cloud CLI
 # https://cloud.google.com/sdk/docs/install-sdk?hl=de#rpm
-sudo tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
 [google-cloud-cli]
 name=Google Cloud CLI
 baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el8-x86_64
@@ -45,8 +48,8 @@ gpgcheck=1
 repo_gpgcheck=0
 gpgkey=https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
 EOM
-sudo dnf install -y libxcrypt-compat.x86_64 \
-  && sudo dnf install -y google-cloud-cli google-cloud-cli-skaffold google-cloud-cli-gke-gcloud-auth-plugin 
+dnf install -y libxcrypt-compat.x86_64 \
+  && dnf install -y google-cloud-cli google-cloud-cli-skaffold google-cloud-cli-gke-gcloud-auth-plugin 
   # && gcloud init
 
 # Programs that need to be installed manually
