452922b80271c1dfcfe25e2fb010886cb52f20a9
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-11-21 00:58:11 +0100

    feat(GNOME): Add mouse/touchpad settings

--- dot_scripts/executable_fedora_gnome_install_user.sh
+++ dot_scripts/executable_fedora_gnome_install_user.sh
@@ -13,6 +13,10 @@ gsettings set org.gnome.mutter.wayland.keybindings restore-shortcuts [] \
 gsettings set org.gnome.settings-daemon.plugins.media-keys home "['<Super>e']" # Open file explorer
 gsettings set org.gnome.desktop.wm.keybindings switch-group [] \
   && gsettings set org.gnome.desktop.wm.keybindings cycle-group "['<Alt>grave']" # Switch between windows of an app directly
+gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat # Disable mouse acceleration
+gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false # Disable natural scrolling for mice
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true # Tap touchpad to click
+gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false # Disable natural scrolling for touchpad
 pip3 install --upgrade gnome-extensions-cli
 
 # Interactive / manual commands at the end
