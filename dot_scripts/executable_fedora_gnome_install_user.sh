f0f48074e7546d33a0cea09be7ddac143952abcb
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2023-11-21 01:45:03 +0100

    feat(GNOME): Add misc settings
    Night light. Power. Trash cleanup etc.

--- dot_scripts/executable_fedora_gnome_install_user.sh
+++ dot_scripts/executable_fedora_gnome_install_user.sh
@@ -17,6 +17,15 @@ gsettings set org.gnome.desktop.peripherals.mouse accel-profile flat # Disable m
 gsettings set org.gnome.desktop.peripherals.mouse natural-scroll false # Disable natural scrolling for mice
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click true # Tap touchpad to click
 gsettings set org.gnome.desktop.peripherals.touchpad natural-scroll false # Disable natural scrolling for touchpad
+gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true # Enable Night Light
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-ac-timeout 7200 # Automatic Suspend on AC power after 2h
+gsettings set org.gnome.settings-daemon.plugins.power sleep-inactive-battery-timeout 1800 # Automatic Suspend on battery power after 30m
+gsettings set org.gnome.desktop.session idle-delay 900 # Turn screen blank after 15m
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim false # Disable screen dimming because it happens way too quickly (30s), which seemingly can't be changed
+gsettings set org.gnome.desktop.screensaver lock-delay 30 # Lock screen 30s after going blank
+gsettings set org.gnome.desktop.privacy recent-files-max-age 30 # Only remember recently used files for 30 days
+gsettings set org.gnome.desktop.privacy remove-old-trash-files true # Automatically delete trash
+gsettings set org.gnome.desktop.privacy remove-old-temp-files true # Automatically delete temp files
 pip3 install --upgrade gnome-extensions-cli
 
 # Interactive / manual commands at the end
