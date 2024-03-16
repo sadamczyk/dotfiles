4c4180dc98f8bd902f425a66700d544dabc43dad
Sebastian Adamczyk <sebastian.adamczyk@even-on-sunday.com>
2024-03-16 18:05:00 +0100

    fix: Replace exa with eza
    exa is unmaintained. eza is it's replacement.
    https://www.github.com/ogham/exa/issues/1243

--- dot_config/private_fish/functions/l.fish
+++ dot_config/private_fish/functions/l.fish
@@ -1,3 +1,3 @@
 function l --wraps ls --description "List contents of directory using long format"
-    exa -l $argv
+    eza -l $argv
 end
--- dot_config/private_fish/functions/ll.fish
+++ dot_config/private_fish/functions/ll.fish
@@ -1,3 +1,3 @@
 function ll --wraps ls --description "List contents of directory, including hidden files in directory using long format"
-    exa -la $argv
+    eza -la $argv
 end
